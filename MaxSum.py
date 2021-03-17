2328ec230e0a84f75c57fa61c0385e911a464e2a
Mor <63946146+morn246@users.noreply.github.com>
2021-03-17 11:12:18 +0200

    Update MaxSum.py

--- MaxSum.py
+++ MaxSum.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-A =  [-2, 1, -3, 5, -2, 1, 2, -6, 5]
 
-for i in range(1, len(A)):
-    A[i] = max(A[i-1] + A[i],A[i])   #keep the sum larger untill A[i] 
-       
-maxSum= max(A) #the max value in the A
-print('Maximum Sum: ', maxSum )  
+def maxSubArray(A): 
+    for i in range(1, len(A)):
+        A[i] = max(A[i-1] + A[i],A[i])   #keep the sum larger untill A[i] 
+    return max(A) #the max value in the A
+A =  [-2, 1, -3, 5, -2, 1, 2, -6, 5]       
+print('Maximum Sum: ', maxSubArray(A) )  
