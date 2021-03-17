8af1eae008dd9cafcf9fcc5e62458c04095320e3
Mor <63946146+morn246@users.noreply.github.com>
2021-03-17 11:52:52 +0200

    Update MaxSum.py

--- MaxSum.py
+++ MaxSum.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+'''Given an  Array consisting of  integers, Return an integer representing the maximum possible sum of the contiguous subarray
+Exemple:
+Input:    A =  [-2, 1, -3, 5, -2, 1, 2, -6, 5]
+Output:  6 {The subarray [5, -2, 1, 2] }'''
+
 def maxSumSubArray(A): 
     for i in range(1, len(A)):
         A[i] = max(A[i-1] + A[i],A[i])   #keep the sum larger untill A[i] 
