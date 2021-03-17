bb2753ed00e6deab4696f740fc00b250c92e1af8
Mor <63946146+morn246@users.noreply.github.com>
2021-03-17 11:53:41 +0200

    Update MaxSum.py

--- MaxSum.py
+++ MaxSum.py
@@ -7,7 +7,7 @@ Output:  6 {The subarray [5, -2, 1, 2] }'''
 def maxSumSubArray(A): 
     for i in range(1, len(A)):
         A[i] = max(A[i-1] + A[i],A[i])   #keep the sum larger untill A[i] 
-    return max(A) #the max value in the A
+    return max(A) #the max value in A
 A =  [-2, 1, -3, 5, -2, 1, 2, -6, 5]       
 print('Maximum Sum:', maxSumSubArray(A) )  
 # Maximum Sum: 6
